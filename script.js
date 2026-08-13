9df4fc6602536990959608c1d46bc6f0f880b4cc
DaudBD <arifuldaud@gmail.coml>
2026-08-13 20:19:04 +0600

    Add JavaScript practice codes

---
+++ script.js
@@ -0,0 +1,17 @@
+//document.getElementById('demo').innerHTML = 'Hello JavaScript';
+
+//let sum= 90+40;
+//document.getElementById('demo').innerHTML = 'The sum is: ' + sum;
+
+let sum= 90+40;
+console.log(sum);
+
+if (sum > 100) {
+    console.log('Expensive');
+} else {
+    console.log('Cheap');
+}
+
+//document.getElementById('myImage').src='pic_bulbon.gif'";
+
+//document.getElementById('myImage').src='pic_bulboff.gif'";
